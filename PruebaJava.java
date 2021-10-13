c11b98237d6b3fd00dabe4c611382357ccec93a4
Myrian <myrianhf95@gmail.com>
2021-10-13 13:59:11 +0200

    Cantante favorito

--- PruebaJava.java
+++ PruebaJava.java
@@ -1,12 +1,12 @@
 public class PruebaJava {
 
-	public stativ void main(String args[]) {
+	public static void main(String args[]) {
 
-		System.out.println("Myrian Herreras Franco");
 		System.out.println("Cembranos 1995");
 		System.out.println("Cembranos - Windows 10");
-		System.out.println("Cembranos - Windows 8")
-		System.out.println("Me gusta el deporte")
+		System.out.println("Cembranos - Windows 8");
+		System.out.println("Me gusta el deporte");
+		System.out.println("Ed Sheeran");
 
 	}
 
